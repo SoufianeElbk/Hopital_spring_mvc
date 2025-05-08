ad9b2a3eb2854efc5583082c04a782cf391fd659
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-09 00:37:23 +0100

    Spring security : JDBC Authentication

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -7,9 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.Date;
 
 @SpringBootApplication
@@ -51,6 +55,46 @@ public class HopitalApplication implements CommandLineRunner {
     @Bean
     PasswordEncoder passwordEncoder() {
         return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder = passwordEncoder();
+//        return args -> {
+//
+//            UserDetails u1 = jdbcUserDetailsManager.loadUserByUsername("user11");
+//            if (u1 == null) {
+//                jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build());
+//            }
+//
+//            UserDetails u2 = jdbcUserDetailsManager.loadUserByUsername("user22");
+//            if (u2 == null) {
+//                jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build());
+//            }
+//
+//            UserDetails a2 = jdbcUserDetailsManager.loadUserByUsername("admin2");
+//            if (a2 == null) {
+//                jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build());
+//            }
+//
+//        };
+
+        return args -> {
+            if (!jdbcUserDetailsManager.userExists("user11")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build());
+            }
+            if (!jdbcUserDetailsManager.userExists("user22")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build());
+            }
+            if (!jdbcUserDetailsManager.userExists("admin2")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build());
+            }
+        };
     }
 
 }
--- src/main/java/ma/enset/hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/hopital/security/SecurityConfig.java
@@ -14,7 +14,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +26,11 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+//    @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
         String encodedPassword = passwordEncoder.encode("1234");
         System.out.println(encodedPassword);
@@ -36,7 +43,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin().loginPage("/login").permitAll();
+        httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll();
         httpSecurity.authorizeHttpRequests().requestMatchers("/webjars/**").permitAll();
 //        httpSecurity.authorizeHttpRequests().requestMatchers("/user/**").hasRole("USER");
 //        httpSecurity.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN");
